deca262f7173c0f40d040f3e552c3918205b5d9f
guojiguang <1135899486@qq.com>
2020-03-12 17:35:36 +0800

    twitter分享功能完成

--- src/views/nested/menu2/index.vue
+++ src/views/nested/menu2/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div style="padding: 30px;">
-    <p @click="twitterShare">twitter分享</p>
+    <a :href="tw_href" @click="twitterShare">twitter分享</a>
     <p @click="facebookShare">faceBook分享</p>
     <p>分享3</p>
     <p>分享4</p>
@@ -15,13 +15,13 @@
         // twitter
         href_com: 'https://bdx.ysxapp.com/#/login?invite_code=' + 'N98654',// twitter分享链接
         text_com: 'Welcome to the Big Dipper multilingual pc website -> My invitation code: ' + 'N98654',// twitter分享标题
+        tw_href:'',
         // facebook
         href_com1: 'https://bdx.ysxapp.com/#/login?invite_code=' + 'N98654',
         text_com1: 'Welcome to the Big Dipper multilingual pc website -> My invitation code: ' + 'N98654',
       }
     },
     methods: {
-
       twitterShare() {
         // twitter 分享链接
         this.tw_href = `javascript:window.open('https://twitter.com/share?text=` + this.text_com + `&url=` + this.href_com + `','_blank');void(0)`
