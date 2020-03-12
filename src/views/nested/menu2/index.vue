5e48592665580bc1c0d4abb82a9e3c06cba208c8
guojiguang <1135899486@qq.com>
2020-03-12 15:38:55 +0800

    配置twitter分享成功

--- src/views/nested/menu2/index.vue
+++ src/views/nested/menu2/index.vue
@@ -1,11 +1,43 @@
 <template>
   <div style="padding: 30px;">
-    <el-alert :closable="false" title="menu 2"/>
+    <p @click="twitterShare">twitter分享</p>
+    <p @click="facebookShare">faceBook分享</p>
+    <p>分享3</p>
+    <p>分享4</p>
+    <p>分享5</p>
   </div>
 </template>
 
 <script>
-    export default {}
+  export default {
+    data() {
+      return {
+        // twitter
+        href_com: 'https://bdx.ysxapp.com/#/login?invite_code=' + 'N98654',// twitter分享链接
+        text_com: 'Welcome to the Big Dipper multilingual pc website -> My invitation code: ' + 'N98654',// twitter分享标题
+        // facebook
+        href_com1: 'https://bdx.ysxapp.com/#/login?invite_code=' + 'N98654',
+        text_com1: 'Welcome to the Big Dipper multilingual pc website -> My invitation code: ' + 'N98654',
+      }
+    },
+    methods: {
+
+      twitterShare() {
+        // twitter 分享链接
+        this.tw_href = `javascript:window.open('https://twitter.com/share?text=` + this.text_com + `&url=` + this.href_com + `','_blank');void(0)`
+      },
+      facebookShare() {
+        let _this = this
+        FB.ui({
+          method: 'share',
+          href: _this.href_com1,
+          quote: this.text_com1
+        }, function (response) {
+          // console.log('response', response)
+        });
+      },
+    },
+  }
 </script>
 
 <style scoped>
