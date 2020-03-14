149d17efe13d1b28e4a1ae4e9a531f20f2a56db7
guojiguang <1135899486@qq.com>
2020-03-14 17:43:54 +0800

    whatsup分享测试

--- src/views/nested/menu2/index.vue
+++ src/views/nested/menu2/index.vue
@@ -2,8 +2,8 @@
   <div style="padding: 30px;">
     <a :href="tw_href" @click="twitterShare">twitter分享</a>
     <p @click="facebookShare">faceBook分享</p>
-    <p>分享3</p>
-    <p>分享4</p>
+    <p @click="shareWa" class="xq-whatsapp" data-action="share/whatsapp/share">whatsapp分享</p>
+    <p @click="shareWa">分享4</p>
     <p>分享5</p>
   </div>
 </template>
@@ -14,18 +14,20 @@
       return {
         // twitter
         href_com: 'https://bdx.ysxapp.com/#/login?invite_code=' + 'N98654',// twitter分享链接
-        text_com: 'Welcome to the Big Dipper multilingual pc website -> My invitation code: ' + 'N98654',// twitter分享标题
-        tw_href:'',
+        text_com: '测试内容: ' + 'N98654',// twitter分享标题
+        tw_href: '',
         // facebook
         href_com1: 'https://bdx.ysxapp.com/#/login?invite_code=' + 'N98654',
         text_com1: 'Welcome to the Big Dipper multilingual pc website -> My invitation code: ' + 'N98654',
       }
     },
     methods: {
+      /* twitter分享 */
       twitterShare() {
         // twitter 分享链接
         this.tw_href = `javascript:window.open('https://twitter.com/share?text=` + this.text_com + `&url=` + this.href_com + `','_blank');void(0)`
       },
+      /* facebook分享 */
       facebookShare() {
         let _this = this
         FB.ui({
@@ -35,6 +37,15 @@
         }, function (response) {
           // console.log('response', response)
         });
+      },
+      /* whatsapp分享 */
+      shareWa(str) {
+        // content是我们自己定义的一些需要分享的内容;
+        let u = document.getElementsByClassName("share_url")[0].content;
+        console.log(u,'u')
+        let t = document.getElementsByClassName("share_title")[0].content;
+        console.log(t,'t')
+        window.open ("http://web.whatsapp.com/send?text=" + encodeURIComponent(t) + encodeURIComponent("nn" + u) + "&via=lopscoop");
       },
     },
   }
