60c9f7f721567b317adbc831e044500cdc5d2d3b
郭吉光 <18434392328@163.com>
2020-04-23 17:23:41 +0800

    按钮组件封装完成

--- src/views/vuestudy/eleComponents/index.vue
+++ src/views/vuestudy/eleComponents/index.vue
@@ -1,7 +1,14 @@
 <template>
   <div>
-    eleComponents
-    <CustomBtn></CustomBtn>
+    <el-alert
+      title="自定义按钮组件"
+      type="success"
+      effect="dark"/>
+    <CustomBtn
+      type="primary">
+      按钮
+    </CustomBtn>
+    <el-button type="primary">按钮</el-button>
   </div>
 </template>
 
