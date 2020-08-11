5e01537c73881b7d4d1ab648efc9231fd949ffb0
郭吉光 <18434392328@163.com>
2020-08-11 17:28:11 +0800

    添加自定义 输入框组件

--- src/eleComponents/components.js
+++ src/eleComponents/components.js
@@ -6,3 +6,7 @@ export {
   default as CustomSwitch
 }
   from "./CustomSwitch";
+export {
+  default as CustomInput
+}
+  from "./CustomInput";
--- src/views/vuestudy/eleComponents/index.vue
+++ src/views/vuestudy/eleComponents/index.vue
@@ -7,9 +7,9 @@
       effect="dark"/>
     <CustomBtn
       type="primary">
-      按钮
+      自定义
     </CustomBtn>
-    <el-button type="primary" id="testBtn">按钮</el-button>
+    <el-button type="primary" id="testBtn">el-按钮</el-button>
     <!-- 自定义switch组件 -->
     <el-alert
       title="自定义switch组件"
@@ -27,33 +27,42 @@
       active-color="#13ce66"
       inactive-color="#ff4949">
     </el-switch>
-
+    <!-- 自定义input组件 -->
+    <el-alert
+      title="自定义input组件"
+      type="success"
+      effect="dark"/>
+    <CustomInput v-model="cusInput" style="width: 200px;" placeholder="自定义输入框"/>
+    <el-input v-model="elInput" style="width: 200px;" placeholder="el 输入框"/>
   </div>
 </template>
 
 <script>
-  import {CustomBtn, CustomSwitch} from '_ele/components'
+import {CustomBtn, CustomSwitch, CustomInput} from '_ele/components'
 
-  export default {
-    name: 'eleComponents',
-    components: {CustomBtn, CustomSwitch},
-    data() {
-      return {
-        /* switch组件参数 */
-        switchValue: true,
-        elValue: true,
-      };
-    },
-    methods: {
-      testClick() {
-        console.log(document.getElementById('testBtn'))
-      }
-    },
-    mounted() {
-      // 调用dispatch的获取元素方法
-      this.$on('getTestBtn', this.testClick);
+export default {
+  name: 'eleComponents',
+  components: {CustomBtn, CustomSwitch, CustomInput},
+  data() {
+    return {
+      /* switch组件参数 */
+      switchValue: true,
+      elValue: true,
+      /* input组件参数 */
+      elInput: '',
+      cusInput: ''
+    };
+  },
+  methods: {
+    testClick() {
+      console.log(document.getElementById('testBtn'))
     }
+  },
+  mounted() {
+    // 调用dispatch的获取元素方法
+    this.$on('getTestBtn', this.testClick);
   }
+}
 </script>
 
 <style scoped>
