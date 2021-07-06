0494daf7d0262f9aa527b85bc1b0ae09658b1657
guojiguang <41253028+btWin9527@users.noreply.github.com>
2021-07-06 15:46:51 +0800

    添加classIdx测试展示

--- src/views/vuestudy/classIdx/index.vue
+++ src/views/vuestudy/classIdx/index.vue
@@ -105,13 +105,18 @@
       <!-- classIdx 502 -->
       <div class="item-cont" v-if="item.classIdx === 502">
         <h4>classIdx{{ item.classIdx }}</h4>
-        <h4 style="font-weight: bold">答案:</h4>
         <div
           class="mb-10"
           v-for="(itemAnswer, itemIdx) in item.jsonAnswers"
           :key="itemIdx"
         >
-          $${{ itemAnswer.suggestedAnswer }}$$
+          <h4>{{itemIdx+1}}. 题干</h4>
+          <div>$${{ itemAnswer.content }}$$</div>
+          <h4 style="font-weight: bold">{{itemIdx+1}}. 答案:</h4>
+          <div>
+            $${{ itemAnswer.suggestedAnswer }}$$
+          </div>
+
         </div>
       </div>
       <!-- classIdx 407 -->
@@ -123,24 +128,123 @@
             class="mb-10"
             v-for="(itemAnswer, itemIdx) in item.jsonAnswers"
             :key="itemIdx"
-          >{{ itemAnswer.suggestedAnswer }}</div
           >
+            {{ itemAnswer.suggestedAnswer }}
+          </div>
+        </div>
+      </div>
+      <!-- classIdx 417  解析异常-->
+      <div class="item-cont" v-if="item.classIdx === 417">
+        <h4>classIdx{{ item.classIdx }}</h4>
+        <h4 style="font-weight: bold">答案:</h4>
+        <p>备注:拼音不解析</p>
+        <div class="item-answer pinyin-answer">
+          <div
+            v-for="(itemAnswer, itemIdx) in item.jsonAnswers"
+            :key="itemIdx"
+            class="pinyin-cont"
+          >
+            <div class="mr-10">{{ itemIdx + 1 }}.</div>
+            <div
+              v-for="(itemPinYin, idxPinYin) in itemAnswer.subAnswers"
+              :key="idxPinYin"
+              class="mr-10"
+            >
+              <div></div>
+              <div>{{ handlePinyin(itemPinYin.suggestedAnswer)[1] }}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- classIdx 600 -->
+      <div class="item-cont" v-if="item.classIdx === 600">
+        <h4>classIdx{{ item.classIdx }}</h4>
+        <h4 style="font-weight: bold">答案:</h4>
+        <div class="item-answer">
+          <div
+            class="mb-10"
+            v-for="(itemAnswer, itemIdx) in item.jsonAnswers"
+            :key="itemIdx"
+          >
+            <span class="mr-10">({{ itemIdx + 1 }})</span>
+            <span>{{ itemAnswer.suggestedAnswer }}</span>
+          </div>
+        </div>
+      </div>
+      <!-- classIdx 606 或 416 或 602 -->
+      <div
+        class="item-cont"
+        v-if="
+          item.classIdx === 606 ||
+            item.classIdx === 416 ||
+            item.classIdx === 602
+        "
+      >
+        <h4>classIdx{{ item.classIdx }}</h4>
+        <h4 style="font-weight: bold">答案:</h4>
+        <div class="item-answer">
+          <div
+            class="mb-10"
+            v-for="(itemAnswer, itemIdx) in item.jsonAnswers"
+            :key="itemIdx"
+          >
+            <span class="mr-10">{{ itemIdx + 1 }}.</span>
+            <span>{{ itemAnswer.suggestedAnswer }}</span>
+          </div>
+        </div>
+      </div>
+      <!-- classIdx 414 -->
+      <div class="item-cont" v-if="item.classIdx === 414">
+        <h4>classIdx{{ item.classIdx }}</h4>
+        <h4 style="font-weight: bold">答案:</h4>
+        <div class="item-answer pinyin-answer">
+          <div
+            v-for="(itemAnswer, itemIdx) in item.jsonAnswers"
+            :key="itemIdx"
+            class="pinyin-cont"
+          >
+            <div class="pinyin-idx">{{ itemIdx + 1 }}.</div>
+            <div>
+              <div>{{ handlePinyin(itemAnswer.suggestedAnswer)[1] }}</div>
+              <div>{{ handlePinyin(itemAnswer.suggestedAnswer)[0] }}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- classIdx 406 -->
+      <div class="item-cont" v-if="item.classIdx === 406">
+        <h4>classIdx{{ item.classIdx }}</h4>
+        <h4 style="font-weight: bold">答案:</h4>
+        <p>题目中存在错别字的词语如下：</p>
+        <div class="item-answer">
+          <div
+            class="mb-10"
+            v-for="(itemAnswer, itemIdx) in item.jsonAnswers"
+            :key="itemIdx"
+          >
+            {{ itemAnswer.suggestedAnswer }}
+          </div>
+        </div>
+      </div>
+      <!-- classIdx 402 或 401 -->
+      <div
+        class="item-cont"
+        v-if="item.classIdx === 402 || item.classIdx === 401"
+      >
+        <h4>classIdx{{ item.classIdx }}</h4>
+        <h4 style="font-weight: bold">答案:</h4>
+        <div class="item-answer">
+          <div
+            class="mb-10"
+            v-for="(itemAnswer, itemIdx) in item.jsonAnswers"
+            :key="itemIdx"
+          >
+            {{ itemAnswer.suggestedAnswer }}
+          </div>
         </div>
       </div>
     </div>
   </div>
-  <!--          &lt;!&ndash; classIdx 607 || classIdx 400 || classIdx 404 ||  classIdx 405 &ndash;&gt;-->
-  <!--          <div v-if="item.classIdx === 607">-->
-  <!--            <div class="item-answer">-->
-  <!--              {{ itemAnswer.suggestedAnswer }}-->
-  <!--            </div>-->
-  <!--          </div>-->
-  <!--          &lt;!&ndash; classIdx 501 &ndash;&gt;-->
-  <!--          <div v-if="item.classIdx === 501">-->
-  <!--            <span class="item-answer">-->
-  <!--              {{ itemAnswer.suggestedAnswer }}-->
-  <!--            </span>-->
-  <!--          </div>-->
 </template>
 <script>
 import { classIdxData } from "../../../api/article";
@@ -165,7 +269,7 @@ export default {
             return itemAnswer;
           });
         });
-        console.log(this.jsonAnswersArr, "  this.jsonAnswersArr");
+        console.log(this.jsonAnswersArr, " this.jsonAnswersAr");
         this.latexAnalysis();
       });
     },
@@ -215,5 +319,8 @@ export default {
 }
 .mb-10 {
   margin-bottom: 10px;
+}
+.mr-10 {
+  margin-right: 10px;
 }
 </style>
